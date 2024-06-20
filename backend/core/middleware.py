c5813b55cf06843520a48faa8e941ff3258265c8
katya <edgotskina@greenatom.ru>
2024-06-20 11:10:09 +0300

    added redis + auth edited

--- backend/core/middleware.py
+++ backend/core/middleware.py
@@ -9,8 +9,8 @@ from backend.core.config import settings
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
-        auth_key = request.headers.get('auth_key')
-        if isinstance(auth_key, str) and auth_key != settings.AUTH_KEY and str(request.url).split('/')[-1] not in ['openapi.json ', 'swagger', 'metrics']:
+        auth_key = request.headers.get('Authorization')
+        if isinstance(auth_key, str) and auth_key != f"Bearer {settings.AUTH_KEY}" and str(request.url).split('/')[-1] not in ['openapi.json ', 'swagger', 'metrics']:
             return JSONResponse(content={'err': "No auth header"}, status_code=status.HTTP_401_UNAUTHORIZED)
         response = await call_next(request)
         return response
