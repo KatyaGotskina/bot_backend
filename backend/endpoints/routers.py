4816dc24eecef8b338bc46c6a8248d54306b61a5
katya <edgotskina@greenatom.ru>
2024-06-26 21:28:25 +0300

    edits

--- backend/endpoints/routers.py
+++ backend/endpoints/routers.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
 
-task_router = APIRouter(prefix='/task')
-category_router = APIRouter(prefix='/category')
-user_router = APIRouter(prefix='/user')
+task_router = APIRouter(prefix='/api/task')
+category_router = APIRouter(prefix='/api/category')
+user_router = APIRouter(prefix='/api/user')
