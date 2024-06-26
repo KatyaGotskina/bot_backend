c1098ecae1c81bd7ed3dddc566aa664c1bb9f77a
katya <edgotskina@greenatom.ru>
2024-06-26 21:34:40 +0300

    edits

--- backend/core/middleware.py
+++ backend/core/middleware.py
@@ -10,6 +10,10 @@ from backend.core.config import settings
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
         auth_key = request.headers.get('Authorization')
+        print()
+        print()
+        print()
+        print(auth_key)
         if isinstance(auth_key, str) and auth_key != f"Bearer {settings.AUTH_KEY}" and str(request.url).split('/')[-1] not in ['openapi.json ', 'swagger', 'metrics']:
             return JSONResponse(content={'err': "No auth header"}, status_code=status.HTTP_401_UNAUTHORIZED)
         response = await call_next(request)
